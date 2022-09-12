0de1eadeaaa39653329a8c7565ac2cbe662faaff
gomcode <xorb.taegyu@gmail.com>
2022-09-12 12:59:07 +0900

    Initial commit

---
+++ src/main/java/com/team6/tinder/TinderApplication.java
@@ -0,0 +1,13 @@
+package com.team6.tinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderApplication.class, args);
+	}
+
+}
